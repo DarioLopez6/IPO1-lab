88b71b36be6aefeaac5df82480ba5b256e6f01ee
DarioLopez6 <dario.lopez1@alu.uclm.es>
2025-11-23 16:59:31 +0100

    diseño de ventana pedidos termiando

--- Lab-IPO1/obj/Debug/net8.0-windows/LabIPO1.AssemblyInfo.cs
+++ Lab-IPO1/obj/Debug/net8.0-windows/LabIPO1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabIPO1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e349e26485f5611f7553b3d42059d4943de92c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9678eb9e28a0354b6d280321c3899c612be4b3c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabIPO1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabIPO1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
