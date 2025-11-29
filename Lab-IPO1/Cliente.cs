06ec9468d498f0cf51a33d84f7b90752cdae1ed5
DarioLopez6 <dario.lopez1@alu.uclm.es>
2025-11-29 17:01:21 +0100

    pestaña pedidos y clientes funcionales

---
+++ Lab-IPO1/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_IPO1
+{
+    enum FORMAPAGO { TARGETA, EFECTIVO }
+    class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public List<String> Direccion { get; set; }
+        public List<String> Telefono { get; set; }
+        public List<String> eMail { get; set; }
+        public List<String> Alergias { get; set; }
+        public List<String> Intolerancias { get; set; }
+        public List<Pedido> Historial { get; set; }
+        public FORMAPAGO pago { get; set; }
+        public int puntosAcumulados { get; set; }
+        public int puntosCangeados { get; set; }
+        public Cliente(int iD, string nombre, string apellidos, List<String> direccion, List<String> telefono, List<String> eMail, List<String> alergias, List<String> intolerancias, FORMAPAGO pago, int puntAcu, int puntCang)
+        {
+            ID = iD;
+            Nombre = nombre;
+            Apellidos = apellidos;
+            Direccion = direccion;
+            Telefono = telefono;
+            this.eMail = eMail;
+            Alergias = alergias;
+            Intolerancias = intolerancias;
+            this.pago = pago;
+            Historial = null;
+            puntosAcumulados = puntAcu;
+            puntosCangeados = puntCang;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- Lab-IPO1/obj/Release/net8.0-windows/LabIPO1.AssemblyInfo.cs
+++ Lab-IPO1/obj/Release/net8.0-windows/LabIPO1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabIPO1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d494d289b2279a104bb4d98decb7394c1bfde8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6243491e901bc68955659e5771c6ce89f68d460")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabIPO1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabIPO1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
