bc476c5fa938046bb6d654764d8fe163ef7ac8e9
DarioLopez6 <dario.lopez1@alu.uclm.es>
2025-12-07 11:14:56 +0100

    Ejemplo basico de cambio d fondo

--- Lab-IPO1/SettingsWindow.xaml.cs
+++ Lab-IPO1/SettingsWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Media;
+﻿using System.Windows;
 
 namespace Lab_IPO1
 {
@@ -11,61 +9,15 @@ namespace Lab_IPO1
             InitializeComponent();
         }
 
-        private void BtnPrimaryColor_Click(object sender, RoutedEventArgs e)
+        private void BtnLight_Click(object sender, RoutedEventArgs e)
         {
-            if (TryParseColor(txtPrimaryColor.Text, out Color color))
-                ThemeManager.SetPrimaryColor(color);
+            ThemeManager.SetLightTheme();
         }
 
-        private void BtnSecondaryColor_Click(object sender, RoutedEventArgs e)
+        private void BtnDark_Click(object sender, RoutedEventArgs e)
         {
-            if (TryParseColor(txtSecondaryColor.Text, out Color color))
-                ThemeManager.SetSecondaryColor(color);
+            ThemeManager.SetDarkTheme();
         }
 
-        private void BtnTextColor_Click(object sender, RoutedEventArgs e)
-        {
-            if (TryParseColor(txtTextColor.Text, out Color color))
-                ThemeManager.SetTextColor(color);
-        }
-
-        private void BtnFontSize_Click(object sender, RoutedEventArgs e)
-        {
-            if (double.TryParse(txtFontSize.Text, out double size))
-                ThemeManager.SetFontSize(size);
-        }
-
-        private void CbFontFamily_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            if (cbFontFamily.SelectedItem is System.Windows.Controls.ComboBoxItem item)
-                ThemeManager.SetFontFamily(item.Content.ToString());
-        }
-
-        private void BtnReset_Click(object sender, RoutedEventArgs e)
-        {
-            ThemeManager.ResetDefaults();
-
-            // Actualizar campos visuales
-            txtPrimaryColor.Text = "#FFFF6B35";
-            txtSecondaryColor.Text = "#F0F0F0";
-            txtTextColor.Text = "Black";
-            txtFontSize.Text = "14";
-            cbFontFamily.SelectedIndex = 0;
-        }
-
-        private bool TryParseColor(string input, out Color color)
-        {
-            try
-            {
-                color = (Color)ColorConverter.ConvertFromString(input);
-                return true;
-            }
-            catch
-            {
-                MessageBox.Show("Color inválido. Usa formato #RRGGBB o nombre de color.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-                color = Colors.Transparent;
-                return false;
-            }
-        }
     }
 }
